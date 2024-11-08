dd0219d73f763b5152f63289d9a8a512cbd5d816
Dana Senko <dana.es@mail.ru>
2024-11-08 11:20:42 +0300

    upgrade parser file

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -1,6 +1,13 @@
 import json
+import yaml
+import os
 
 
-def read_json_file(filepath):
+def parse_file(filepath):
+    _, end_of_file = os.path.splitext(filepath)
+
     with open(filepath, "r") as file:
-        return json.load(file)
+        if end_of_file == ".json":
+            return json.load(file)
+        elif end_of_file in {".yaml", ".yml"}:
+            return yaml.safe_load(file)
