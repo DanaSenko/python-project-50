6f593be30d46bb7564a34a45221a248d1ae29843
Dana Senko <dana.es@mail.ru>
2024-11-08 11:21:53 +0300

    add pytest.mark.parametrize

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -4,11 +4,24 @@ from gendiff import gendiff
 
 def read_fixture(file_path):
     with open(file_path, "r") as file:
-        return file.read()
+        return file.read().strip()
 
 
-def test_gendiff():
-    with open("tests/fixtures/expected_diff.txt") as expected_file:
-        expected_output = expected_file.read().strip()
-    assert gendiff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == expected_output
-    
+@pytest.mark.parametrize(
+    "file1, file2, expected_output_file",
+    [
+        (
+            "tests/fixtures/file1.json",
+            "tests/fixtures/file2.json",
+            "tests/fixtures/expected_diff.txt",
+        ),
+        (
+            "tests/fixtures/file1.yaml",
+            "tests/fixtures/file2.yaml",
+            "tests/fixtures/expected_diff.txt",
+        ),
+    ],
+)
+def test_gendiff(file1, file2, expected_output_file):
+    expected_output = read_fixture(expected_output_file)
+    assert gendiff(file1, file2) == expected_output
