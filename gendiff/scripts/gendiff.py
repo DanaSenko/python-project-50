bea1a6b06ff80a6e28145b135d8eabe3e56bc610
Dana Senko <dana.es@mail.ru>
2024-11-12 12:06:16 +0300

    update script gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,12 @@
-from gendiff.cli import main
+from gendiff.cli import pars_args
+from gendiff.gendiff import gendiff
+
+
+def main():
+    args = pars_args()
+    diff = gendiff(args.first_file, args.second_file, args.format)
+    print(diff)
+
 
 if __name__ == "__main__":
     main()
