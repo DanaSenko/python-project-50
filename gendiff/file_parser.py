f04e02891825a01e7fa2e6422ee8f19bc4fd9ff1
Dana Senko <dana.es@mail.ru>
2024-11-22 11:51:57 +0300

    update files (add math-case)

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -7,7 +7,8 @@ def parse_file(filepath):
     _, end_of_file = os.path.splitext(filepath)
 
     with open(filepath, "r") as file:
-        if end_of_file == ".json":
-            return json.load(file)
-        elif end_of_file in {".yaml", ".yml"}:
-            return yaml.safe_load(file)
+        match end_of_file:
+            case ".json":
+                return json.load(file)
+            case ".yaml" | ".yml":
+                return yaml.safe_load(file)
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,18 +1,20 @@
 from gendiff.file_parser import parse_file
 from gendiff.build_diff import build_diff
-from formats.stylish import stylish
-from formats.plain import plain
-from formats.json import json_format
+from gendiff.formats.stylish import stylish
+from gendiff.formats.plain import plain
+from gendiff.formats.json import json_format
 
 
 def generate_diff(filepath1, filepath2, format_name="stylish"):
     dict1 = parse_file(filepath1)
     dict2 = parse_file(filepath2)
     diff = build_diff(dict1, dict2)
-    if format_name == "stylish":
-        return stylish(diff)
-    elif format_name == "plain":
-        return plain(diff)
-    elif format_name == "json":
-        return json_format(diff)
-    raise ValueError
+    match format_name:
+        case "stylish":
+            return stylish(diff)
+        case "plain":
+            return plain(diff)
+        case "json":
+            return json_format(diff)
+        case _:
+            raise ValueError
